80ecefc84950f2e8da4955131527cfa40da56b5b
Spraynard <kellan.martin@gmail.com>
2017-04-01 00:12:31 -0400

    V.5
    So far I have been able to provide a good amount of responsiveness for
    the website using some jQuery built in animations. Talked to the client
    and they felt happy with the visual element.
    
    TO-DO's:
    
    * Use the SimplyBook API to show customer reviews on the site.
    
    * Smooth out the scroll when clicking on a navbar element
    
    * Get contact bar on bottom of site filled out.
    
    * Acquire pictures for site
    
    * Include a lotus flower.... Make it a quick ole .svg
    
    * Write content in text areas.
    
    ^
    |
    |
    Lots of work to do still.
    
    Please enter the commit message for your changes. Lines starting

---
+++ js/main.js
@@ -0,0 +1,100 @@
+massageObjects = [{
+		name : "Sports Massage",
+		description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit est, euismod in rutrum nec, tempus id neque. Nunc a mi sit amet nulla molestie viverra. Aenean dictum interdum tortor vel interdum. Duis commodo elit magna, id lacinia lorem hendrerit eget. Quisque est dolor, tempor quis consequat et, semper eu massa. Suspendisse potenti. Mauris ac ex mauris. Nulla facilisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus consectetur, libero eget consequat feugiat, massa enim sagittis purus, quis laoreet nunc erat et arcu. Duis ante ipsum, dignissim vitae convallis sit amet, imperdiet at mi.",
+		images : ['sports.jpeg'],
+		links : [{name: 'Benefits of Massage For Atheletes - Kerrie Ann Frey', url: 'http://www.healthfitnessmag.com/Health-Fitness-Magazine/February-2013/Benefits-Of-Massage-For-Athletes/'},
+				{name: 'The Role of Massage in Sports Performance and Rehabilitation: Current Evidence and Future Direction - Jason Brummitt', url: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2953308/'}]
+	},
+	{
+		name : "Deep Tissue Massage",
+		description : "deep tissue Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit est, euismod in rutrum nec, tempus id neque. Nunc a mi sit amet nulla molestie viverra. Aenean dictum interdum tortor vel interdum. Duis commodo elit magna, id lacinia lorem hendrerit eget. Quisque est dolor, tempor quis consequat et, semper eu massa. Suspendisse potenti. Mauris ac ex mauris. Nulla facilisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus consectetur, libero eget consequat feugiat, massa enim sagittis purus, quis laoreet nunc erat et arcu. Duis ante ipsum, dignissim vitae convallis sit amet, imperdiet at mi.",
+		images : ["Funnydeeptissue.jpg"],
+		links :[]
+	},
+	{
+		name : "Trigger Point Therapy",
+		description : "Trigger Point Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit est, euismod in rutrum nec, tempus id neque. Nunc a mi sit amet nulla molestie viverra. Aenean dictum interdum tortor vel interdum. Duis commodo elit magna, id lacinia lorem hendrerit eget. Quisque est dolor, tempor quis consequat et, semper eu massa. Suspendisse potenti. Mauris ac ex mauris. Nulla facilisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus consectetur, libero eget consequat feugiat, massa enim sagittis purus, quis laoreet nunc erat et arcu. Duis ante ipsum, dignissim vitae convallis sit amet, imperdiet at mi.",
+		images : ["pressurepoint.jpg"],
+		links : []
+	},
+	{
+		name : "Lymphatic Drainage Massage",
+		description : "Lymphatic Drainage",
+		images : [],
+		links : []
+	},
+	{
+		name : "Pregnancy Massage",
+		description : "Pregnancy Massage",
+		images : [],
+		links : []
+	},
+	{
+		name : "Pre and Post Operation Massage",
+		description : "Pre and Post Op",
+		images : [],
+		links : []
+	}]
+
+var Massage = function (data) {
+	this.name = ko.observable(data.name);
+	this.description = ko.observable(data.description);
+	this.images = ko.observableArray(data.images);
+	this.links = ko.observableArray(data.links);
+}
+
+var viewModel = function () {
+
+	var self = this;
+
+	this.massages = ko.observableArray([]);
+
+	for (m in massageObjects) {
+		this.massages.push(new Massage(massageObjects[m]))
+	}
+
+	this.currentObject = ko.observable(this.massages()[0])
+
+	this.leftNavItems = ["About", "Referrals", "Booking"]
+	this.rightNavItems = ["Contact"]
+
+	this.goTo = function(navItem) {
+		var x, y
+		var about = $("#about-body");
+		var referrals = $("#referrals");
+		var booking = $("#booking-div");
+		var contact = $("#footer-div");
+
+		console.log(navItem)
+
+		var navItemLoc = {
+			"About" : [about.offset().left, about.offset().top - 100],
+			"Referrals" : [referrals.offset().left, referrals.offset().top],
+			"Contact" : [contact.offset().left, contact.offset().top],
+			"Booking" : [booking.offset().left, booking.offset().top]
+		}
+
+		x = navItemLoc[navItem][0]
+		y = navItemLoc[navItem][1]
+
+		console.log("x equals: " + x + ", y equals: " + y)
+		window.scrollTo(x, y);
+	}
+
+	this.switchCurrentObject = function (object) {
+		self.currentObject(object);
+	}
+
+	this.toggleMassagePage = function() {
+		console.log("Hello?");
+		var overlay = $("#overlay");
+		if (overlay.css('display') == 'none') {
+			overlay.show('slow')
+		}
+		else {
+			overlay.hide('fast');
+		}
+	}
+}
+
+ko.applyBindings(new viewModel());
