79ca82f27565946501efb7089ce4c2a6dbb44c44
Spraynard <kellan.martin@gmail.com>
2017-05-03 02:32:46 -0400

    Final Stuff Almost Here... Waiting for Content from Customer

--- js/main.js
+++ js/main.js
@@ -1,58 +1,102 @@
 $(function () {
 	massageObjects = [{
 			name : "Sports Massage",
-			description : "My sports massages are things of fantasy! You've heard about them, but now you can experience first hand the awe-inspiring power of a professional sports massage!\
-							I specifically target the most stressed areas of the body to increase blood flow, release lactic acid, and ultimately help make your muscles heal to be stronger than they ever were before!\
-							All kinds of atheletes, from the olympic professional, to the weekend gym-rat, can get the amazing benefits of my sports massage.",
-			images : ['sports.jpeg'],
+			description : "A massage specific to athletes and the muscles they use in their particular sport. Sports massages help to prevent injury and maintain a \
+				healthy body that performs at the optimum level. The focus is on stretching and lengthening the muscles while keeping the area warm, and \
+				reducing lactic acid build up. Sports massages can be used pre-event, post-event, or just to maintain flexibility and overall health!",
+			images : [{image: 'muscle_guy.jpeg', tags: "Kalamazoo Massage sports massage"},
+					{image: 'sports_massage_volleyball.jpg', tags: "Kalamazoo Massage sports massage"}],
 			links : [{name: 'Benefits of Massage For Atheletes - Kerrie Ann Frey', url: 'http://www.healthfitnessmag.com/Health-Fitness-Magazine/February-2013/Benefits-Of-Massage-For-Athletes/'},
 					{name: 'The Role of Massage in Sports Performance and Rehabilitation: Current Evidence and Future Direction - Jason Brummitt', url: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2953308/'}]
 		},
 		{
+			name: "Children's Massage",
+			description: "I cater my massages to everybody, even your child! Let me help your kid relax and feel the magic \
+				of what a true massage is like. Massages have countless benefits for children, especially kids with ADHD and other behavioral issues. \
+				It's very important to learn to relax when you're stressed out, so let me teach your kid relaxation methods while they lay \
+				on the table. If you want, I can even teach you some techniques to use on your children at home!",
+			images: [{image: "baby_massage.jpg", tags: "Kalamazoo Massage child massage"},
+					{image:"child_massage.jpg", tags: "Kalamazoo Massage child massage"}],
+			links: [{name: "Some Benefits of Massage for Children", url: "http://www.livestrong.com/article/114384-benefits-massage-children/"}]
+		},
+		{
 			name : "Deep Tissue Massage",
-			description : "deep tissue Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit est, euismod in rutrum nec, tempus id neque. Nunc a mi sit amet nulla molestie viverra. Aenean dictum interdum tortor vel interdum. Duis commodo elit magna, id lacinia lorem hendrerit eget. Quisque est dolor, tempor quis consequat et, semper eu massa. Suspendisse potenti. Mauris ac ex mauris. Nulla facilisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus consectetur, libero eget consequat feugiat, massa enim sagittis purus, quis laoreet nunc erat et arcu. Duis ante ipsum, dignissim vitae convallis sit amet, imperdiet at mi.",
-			images : ["Funnydeeptissue.jpg"],
-			links :[]
+			description : "A good deep tissue massage can help with years of pain and help release tension in chronically tense \
+				areas such as the neck, lower back, and shoulders. These types of massages work by physically breaking down \
+				adhesions in the muscle to relieve pain, and restore normal movement. Just to warn you, this type of massage will \
+				probably give discomfort, but the benefits to the discomfort are so worth it.",
+			images : [{image: "Funnydeeptissue.jpg", tags: "Kalamazoo Massage deep tissue massage"}],
+			links :[{name: 'Information about Deep Tissue Massage', url: 'http://www.happymassage.com/wiki/Deep_Tissue_Massage'}]
 		},
 		{
 			name : "Trigger Point Therapy",
-			description : "Trigger Point Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit est, euismod in rutrum nec, tempus id neque. Nunc a mi sit amet nulla molestie viverra. Aenean dictum interdum tortor vel interdum. Duis commodo elit magna, id lacinia lorem hendrerit eget. Quisque est dolor, tempor quis consequat et, semper eu massa. Suspendisse potenti. Mauris ac ex mauris. Nulla facilisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus consectetur, libero eget consequat feugiat, massa enim sagittis purus, quis laoreet nunc erat et arcu. Duis ante ipsum, dignissim vitae convallis sit amet, imperdiet at mi.",
-			images : ["pressurepoint.jpg"],
-			links : []
+			description : "Trigger point therapy is one of the most basic and essential parts of professional massage. \
+				Have you ever heard anyone use the term 'knot' before, when they're talking about their body? Well that's what \
+				a trigger point is, a knot! Trigger point therapy is the process of helping your body release all of that 'bunched up' tension \
+				and let me be the first to tell you that a professional session with me will make you think twice about ever getting your \
+				triger points worked on by anybody else.",
+			images : [{image: "trigger_point_therapy.jpg", tags: "Kalamazoo Massage trigger point therapy"}],
+			links : [{name: 'Myofascial Trigger Point - Wikipedia Article', url: 'https://en.wikipedia.org/wiki/Myofascial_trigger_point'}]
 		},
 		{
 			name : "Lymphatic Drainage Massage",
-			description : "Lymphatic Drainage",
-			images : [],
-			links : []
+			description : "Your body's lymph nodes help to cleanse your blood from toxins that are released throughout the day. \
+				They also help to regulate your immune system! Lymphatic drainage massages help to increase lymph node performance, \
+				and consequently, help to strengthen you immune system. These types of massages are prescribed by doctors after a surgery \
+				on the lymph nodes, but don't let that stop you from asking me to massage these areas. It feels too good not to!",
+			images : [{image :"lymphatic_drainage_massage.jpg", tags:"Kalamazoo Massage lymphatic drainage massage"}],
+			links : [{name: "Information about the Lymphatic System", url:"https://en.wikipedia.org/wiki/Lymphatic_system"}]
 		},
 		{
 			name : "Pregnancy Massage",
-			description : "Pregnancy Massage",
-			images : [],
+			description : "Are you feeling stressed out and tired from carrying all that extra weight? \
+				Can you feel pain and strain on your lower back, or legs? As a mother of two, I know what pregnancy \
+				can do to the body. I also know how to help relieve some of the stress that comes with it! I'll make sure \
+				to make you feel completely comforted as I work on your strained back, legs, and any other muscles that are being used too much. \
+				The inherent benefits on mood from massage will also help you and your partner in these tumultuous times!",
+			images : [{image :"pregnancy_massage_img.jpg", tags: "Kalamazoo Massage pregnancy massage"}],
 			links : []
 		},
 		{
 			name : "Pre and Post Operation Massage",
-			description : "Pre and Post Op",
-			images : [],
-			links : []
+			description : "Surgery is very stressful. I'm here to help relieve that stress, and directly improve\
+				your healing time.\n Massages before any operation help to relieve any stress and anxiety that \
+				you may have, and even help to build the immune system up against those nasty hospital germs you're \
+				going to be exposed to!\n After surgery, massages help healing time dramatically by giving extra blood flow, \
+				and promoting flexibility and mobility in the affected area. In general, you'll also feel better because \
+				massages help promote better overall moods! Basically, if you aren't getting massages before or after your operations \
+				you're really missing out!",
+			images : [{image: "pre_post_surgery_img.jpg", tags: "Kalamazoo Massage Pre and Post Operation Massage"}],
+			links : [{name: '8 Science-Backed Benefits of Massage After Surgery', url: 'http://www.surgerysupplements.com/8-science-backed-benefits-of-massage-after-surgery/'}]
 		}]
 
 	serviceObjects = [
 		{
 			name: "Mossages at the office",
-			description: "We're gonna be office chair massagin!!",
-			images: [],
+			description: "Quite possibly the best way that you can show your employees that you care about them \
+				is by offering them massages at the workplace! I bring my personal massage chair to every in-office massage, \
+				so you can rest easy knowing that your employees are being given the best possible care. If you feel like you want \
+				to set up massages for your employees (and yourself, of course), please give me a call or an e-mail!",
+			images: [{image: "office_massage.jpg", tags: "Kalamazoo Massage Office Massage"}],
 			links: []
 		},
 		{
 			name : "On-Site Event Mossages",
-			description : "I go to your party or outing and just start massaging people for no reason",
-			images: [],
+			description : "I have brought my massage chair to many different types of events, including birthday parties, music festivals, \
+				races, and more. Any event that has people standing for long hours or doing any type of repeated movements is an opportunity for me \
+				to help relax their tired muscles. If you have an event that you feel would be made even better with massages, please call or e-mail me \
+				and we can set something up!",
+			images: [{image: "event_massages.jpg", tags: "Kalamazoo Massage Event Massage"}],
 			links: []
 		}]
 
+	function tagger(tagArray) {
+		var string = "";
+		tagArray.forEach(function(tag) {
+			string += tag + " ";
+		})
+		return string
+	}	
 	var Massage = function (data) {
 		this.name = ko.observable(data.name);
 		this.description = ko.observable(data.description);
